40413d742e2a103d2424a15bb6f8ada835da9ce3
meliwat <muhammedeliwat@gmail.com>
2024-12-31 13:14:31 -0500

    Finished App

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 from flask import Flask
+import random
 
+no = random.randint(1, 9)
+print(no)
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return ('<h1 style="text-align:center">Guess a number between 0 and 9</h1>'
+            '<img src=''https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif''/>')
 
-@app.route("/users/<name>")
-def greetings(name):
-    return "<p>Hello, %s!</p>" % name
+@app.route("/<int:guess>")
+def guess(guess):
+    if guess > no:
+        return "<h1 style='color: purple'>Too high, try again!</h1>" \
+               "<img src='https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif'/>"
+    elif guess < no:
+        return "<h1 style='color: red'>Too low, try again!</h1>" \
+               "<img src='https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif'/>"
+    else:
+     return "<h1 style='color: green'>You found me!</h1>" \
+           "<img src='https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif'/>"
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
